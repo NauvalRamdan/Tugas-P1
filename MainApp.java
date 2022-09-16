63ecdad0ba9b85db4398b2ce7d123842e1fd00ba
NauvalRamdan <113710560+NauvalRamdan@users.noreply.github.com>
2022-09-16 23:39:41 +0700

    Add files via upload

--- MainApp.java
+++ MainApp.java
@@ -1,11 +1,12 @@
 public class MainApp {
+
     public static void main(String[] args) {
-        //panggil class PesawatTelpon
-        PesawatTelpon m = new PesawatTelpon();
+        //panggil class Hewan
+        Hewan m = new Hewan();
         //[nama class] [variabel buatanmu] = new [nama class]();
 
-        m.setTransmiter("Dengan Siapa?");
-        System.out.println("Saya Berbicara "+ m.getTransmiter());
+        m.setMata("Hitam");
+        System.out.println("Mataku "+ m.getMata());
 
     }
 }
